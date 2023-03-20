83ac4bcbc8764b27f9237c6eb07dedc7eaa53785
HunterHarvell <hunterharvell95@gmail.com>
2023-03-19 22:59:19 -0500

    finishes up routes and corresponding index files

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,1 +1,11 @@
 const router = require('express').Router();
+
+// hook to routes
+const usersRoutes = require('./userRoutes');
+const thoughtsRoutes = require('./thoughtRoutes');
+
+router.use('/users', usersRoutes);
+
+router.use('/thoughts', thoughtsRoutes);
+
+module.exports = router;
